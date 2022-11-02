1e39664b96eef21e704262fdfa85adfa08668766
Rritvik <rritvik.kakkar16@gmail.com>
2022-11-03 00:47:01 +0530

    'Changes'

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PokerController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/place-bet', [PokerController::class, 'placeBet'])->name('placeBet');
